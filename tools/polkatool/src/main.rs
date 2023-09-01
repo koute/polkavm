a15253fb646ff39ab7fdd2c7c74a42d1cbc8fe0f
f-gate <felixgate13@gmail.com>
2023-09-01 09:35:21 +0100

    quick impl

--- tools/polkatool/src/main.rs
+++ tools/polkatool/src/main.rs
@@ -13,6 +13,16 @@ enum Args {
         /// The input file.
         input: PathBuf,
     },
+
+    /// Disassembles any .polkavmm blob into its equivelant bytecode.
+    Disassemble {
+        /// The output file.
+        #[clap(short = 'o', long)]
+        output: PathBuf,
+
+        /// The input file.
+        input: PathBuf,
+    },
 }
 
 fn main() {
@@ -38,6 +48,38 @@ fn main() {
 
             if let Err(error) = std::fs::write(&output, blob.as_bytes()) {
                 eprintln!("ERROR: failed to write the program blob to {:?}: {}", output, error);
+                std::process::exit(1);
+            }
+        },
+
+        Args::Disassemble {output, input} => {
+            let config = polkavm::Config::default();
+            let engine = polkavm::Engine::new(&config).unwrap();
+            let data = match std::fs::read(&input) {
+                Ok(data) => data,
+                Err(error) => {
+                    eprintln!("ERROR: failed to read {:?}: {}", input, error);
+                    std::process::exit(1);
+                }
+            };
+            let blob = match polkavm::ProgramBlob::parse(&data[..]) {
+                Ok(b) => b,
+                Err(error) => {
+                    eprintln!("ERROR: failed to parse the raw data into a blob {:?}: {}", input, error);
+                    std::process::exit(1);
+                }
+            };
+            let module = match polkavm::Module::from_blob(&engine, &blob) {
+                Ok(m) => m,
+                Err(error) => { 
+                    eprintln!("ERROR: failed to instantiate a Module from the blob {:?}: {}", input, error);
+                    std::process::exit(1);
+                }
+            };
+            let out = module.display_instructions().join("\n");
+            
+            if let Err(error) = std::fs::write(&output, out) { 
+                eprintln!("ERROR: failed to write the bytecode to {:?}: {}", output, error);
                 std::process::exit(1);
             }
         }
