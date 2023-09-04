d9e5fbb6f50cd14b55c2a1a2d0e2b721362ae6d5
Alexander Theißen <alex.theissen@me.com>
2023-09-04 16:41:11 +0200

    Export extra types (#45)

--- crates/polkavm/src/lib.rs
+++ crates/polkavm/src/lib.rs
@@ -18,8 +18,12 @@ mod compiler_dummy;
 #[cfg(not(all(target_arch = "x86_64", target_os = "linux")))]
 use compiler_dummy as compiler;
 
-pub use polkavm_common::program::ProgramBlob;
-pub use polkavm_common::utils::AsUninitSliceMut;
+pub use polkavm_common::{
+    error::Trap,
+    program::{ProgramBlob, Reg},
+    utils::AsUninitSliceMut,
+};
 
-pub use crate::api::{Caller, Engine, FuncType, Instance, InstancePre, IntoExternFn, Linker, Module, Val, ValType};
+pub use crate::api::{Caller, Engine, Func, FuncType, Instance, InstancePre, IntoExternFn, Linker, Module, TypedFunc, Val, ValType};
 pub use crate::config::Config;
+pub use crate::error::Error;
