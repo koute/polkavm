23998e33ecbea10bb9b7371f166b4d2f2b2e3523
Jan Bujak <jan@parity.io>
2023-09-05 15:05:38 +0000

    Always build guests first when running all of the tests

--- ci/run-all-tests.sh
+++ ci/run-all-tests.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 cd ..
 
+./ci/jobs/build-guests.sh
 ./ci/jobs/build-and-test.sh
 
 case "$OSTYPE" in
   linux*)
     ./ci/jobs/build-and-test-linux.sh
 esac
-
-./ci/jobs/build-guests.sh
 ./ci/jobs/clippy.sh
 ./ci/jobs/rustfmt.sh
 
