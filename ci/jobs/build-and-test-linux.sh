6f952af77bba4bdf517a101cf1f0954aa4eed06f
Jan Bujak <jan@parity.io>
2023-10-17 10:19:57 +0000

    Extend CI tests

--- ci/jobs/build-and-test-linux.sh
+++ ci/jobs/build-and-test-linux.sh
@@ -9,5 +9,14 @@ cd crates/polkavm-zygote
 cargo build --release
 cd ../..
 
-echo ">> cargo run (examples, musl)"
-POLKAVM_TRACE_EXECUTION=1 POLKAVM_ALLOW_INSECURE=1 cargo run --target=i686-unknown-linux-musl -p hello-world-host
+echo ">> cargo run (examples, interpreter, i686-unknown-linux-musl)"
+POLKAVM_TRACE_EXECUTION=1 POLKAVM_ALLOW_INSECURE=1 POLKAVM_BACKEND=interpreter cargo run --target=i686-unknown-linux-musl -p hello-world-host
+
+echo ">> cargo run (examples, interpreter, x86_64-unknown-linux-gnu)"
+POLKAVM_TRACE_EXECUTION=1 POLKAVM_ALLOW_INSECURE=1 POLKAVM_BACKEND=interpreter cargo run --target=x86_64-unknown-linux-gnu -p hello-world-host
+
+echo ">> cargo run (examples, compiler, linux, x86_64-unknown-linux-gnu)"
+POLKAVM_TRACE_EXECUTION=1 POLKAVM_ALLOW_INSECURE=1 POLKAVM_BACKEND=compiler POLKAVM_SANDBOX=linux cargo run --target=x86_64-unknown-linux-gnu -p hello-world-host
+
+echo ">> cargo run (examples, compiler, generic, x86_64-unknown-linux-gnu)"
+POLKAVM_TRACE_EXECUTION=1 POLKAVM_ALLOW_INSECURE=1 POLKAVM_BACKEND=compiler POLKAVM_SANDBOX=generic cargo run --target=x86_64-unknown-linux-gnu -p hello-world-host
