eee157e957570905fb76c07b8adda43ff024fd9b
Jan Bujak <jan@parity.io>
2023-11-22 11:17:12 +0000

    Check that the program blob ends in a control instruction

--- crates/polkavm/src/api.rs
+++ crates/polkavm/src/api.rs
@@ -236,6 +236,27 @@ impl Module {
             }
         }
 
+        {
+            let Some(last_instruction) = blob.instructions().last() else {
+                bail!("the module contains no code");
+            };
+
+            match last_instruction.map_err(Error::from_display)?.op() {
+                Opcode::fallthrough
+                | Opcode::jump_and_link_register
+                | Opcode::trap
+                | Opcode::branch_less_unsigned
+                | Opcode::branch_less_signed
+                | Opcode::branch_greater_or_equal_unsigned
+                | Opcode::branch_greater_or_equal_signed
+                | Opcode::branch_eq
+                | Opcode::branch_not_eq => {}
+                _ => {
+                    bail!("code doesn't end with a control flow instruction")
+                }
+            }
+        }
+
         log::trace!("Parsing jump table...");
         let mut basic_block_by_jump_table_index = Vec::with_capacity(blob.jump_table_upper_bound() + 1);
 
