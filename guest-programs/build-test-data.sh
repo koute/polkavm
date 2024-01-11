ba7faa095239dd9e037bb3cf52f5f85ba802ef25
Jan Bujak <jan@parity.io>
2024-01-11 10:06:10 +0000

    Update the test blob build script

--- guest-programs/build-test-data.sh
+++ guest-programs/build-test-data.sh
@@ -6,7 +6,7 @@ function build_test_data() {
     output_path="../test-data/$1.elf.zst"
 
     echo "> Building: '$1' (-> $output_path)"
-    RUSTFLAGS="-C target-feature=+lui-addi-fusion -C relocation-model=pie -C link-arg=--emit-relocs -C link-arg=-T.cargo/memory.ld --remap-path-prefix=$(pwd)= --remap-path-prefix=$HOME=~" cargo build -q --profile $2 --bin $1 -p $1
+    RUSTFLAGS="-C target-feature=+lui-addi-fusion -C relocation-model=pie -C link-arg=--emit-relocs -C link-arg=--export-dynamic-symbol=__polkavm_symbol_export_hack__* --remap-path-prefix=$(pwd)= --remap-path-prefix=$HOME=~" cargo build -q --profile $2 --bin $1 -p $1
     zstd -f -q -19 -o $output_path target/riscv32ema-unknown-none-elf/$2/$1
     chmod -x $output_path
 }
