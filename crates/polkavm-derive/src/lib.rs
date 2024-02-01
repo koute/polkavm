77d17e5cb3f6ec893eaf8dc763b6db9d6b0f0194
Jan Bujak <jan@parity.io>
2024-02-01 05:35:05 +0000

    Add a dedicated procedural macro crate and make `polkavm-derive` a facade

--- crates/polkavm-derive/src/lib.rs
+++ crates/polkavm-derive/src/lib.rs
@@ -1,26 +1,6 @@
+#![no_std]
 #![doc = include_str!("../README.md")]
 #![forbid(unsafe_code)]
 
-extern crate proc_macro;
-
-use proc_macro::TokenStream;
-
-#[proc_macro_attribute]
-pub fn polkavm_import(args: TokenStream, input: TokenStream) -> TokenStream {
-    syn::parse_macro_input!(args as syn::parse::Nothing);
-    let input = syn::parse_macro_input!(input as syn::ItemForeignMod);
-    match polkavm_derive_impl::polkavm_import(input) {
-        Ok(result) => result.into(),
-        Err(error) => error.into_compile_error().into(),
-    }
-}
-
-#[proc_macro_attribute]
-pub fn polkavm_export(args: TokenStream, input: TokenStream) -> TokenStream {
-    syn::parse_macro_input!(args as syn::parse::Nothing);
-    let input = syn::parse_macro_input!(input as syn::ItemFn);
-    match polkavm_derive_impl::polkavm_export(input) {
-        Ok(result) => result.into(),
-        Err(error) => error.into_compile_error().into(),
-    }
-}
+pub use polkavm_derive_impl_macro::__PRIVATE_DO_NOT_USE_polkavm_import as polkavm_import;
+pub use polkavm_derive_impl_macro::__PRIVATE_DO_NOT_USE_polkavm_export as polkavm_export;
