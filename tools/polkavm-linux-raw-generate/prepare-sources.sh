263ec272f9e1c3dfd6936f35bcbfed9b500cf019
Jan Bujak <jan@parity.io>
2024-08-12 13:31:00 +0000

    linux-raw: update to Linux 6.9.3, add more bindings

--- tools/polkavm-linux-raw-generate/prepare-sources.sh
+++ tools/polkavm-linux-raw-generate/prepare-sources.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 mkdir -p linux
 cd linux
-wget -c "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.5.tar.xz"
-tar -xf linux-6.5.tar.xz
-cd linux-6.5
-make headers_install ARCH=x86_64 INSTALL_HDR_PATH=../linux-6.5-headers
+wget -c "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.9.3.tar.xz"
+tar -xf linux-6.9.3.tar.xz
+cd linux-6.9.3
+make headers_install ARCH=x86_64 INSTALL_HDR_PATH=../linux-6.9.3-headers
