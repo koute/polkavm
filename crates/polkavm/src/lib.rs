cfb756bcd20bf41d9a80643f4ee2eccbba7b7c99
Jan Bujak <jan@parity.io>
2024-09-12 05:23:01 +0000

    Make the generic allocator generic over size, add more tests

--- crates/polkavm/src/lib.rs
+++ crates/polkavm/src/lib.rs
@@ -87,12 +87,14 @@ where
 mod module_cache;
 
 if_compiler_is_supported! {
-    mod bit_mask;
     mod compiler;
     mod sandbox;
 
-    #[cfg(target_os = "linux")]
+    #[cfg(all(target_os = "linux", not(feature = "export-internals-for-testing")))]
     mod generic_allocator;
+
+    #[cfg(all(target_os = "linux", not(feature = "export-internals-for-testing")))]
+    mod bit_mask;
 
     #[cfg(target_os = "linux")]
     mod shm_allocator;
@@ -145,10 +147,22 @@ pub const RETURN_TO_HOST: u32 = polkavm_common::abi::VM_ADDR_RETURN_TO_HOST;
 #[cfg(test)]
 mod tests;
 
+// These need to be toplevel for the macros to work.
+#[cfg(feature = "export-internals-for-testing")]
+pub mod generic_allocator;
+
+#[cfg(feature = "export-internals-for-testing")]
+pub mod bit_mask;
+
+#[cfg(feature = "export-internals-for-testing")]
 #[doc(hidden)]
 pub mod _for_testing {
     #[cfg(target_os = "linux")]
     if_compiler_is_supported! {
         pub use crate::shm_allocator::{ShmAllocation, ShmAllocator};
+        pub fn create_shm_allocator() -> Result<crate::shm_allocator::ShmAllocator, polkavm_linux_raw::Error> {
+            crate::sandbox::init_native_page_size();
+            crate::shm_allocator::ShmAllocator::new()
+        }
     }
 }
